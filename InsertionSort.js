38968519aaeb5e004b869bb265d4d3f07e4405ed
Collin Fairlie <collin.fairlie@gmail.com>
2019-06-25 09:33:26 -0500

    Worked through two for-loops to sort my unsorted array of objects with Insertion Sort.

---
+++ InsertionSort.js
@@ -0,0 +1,55 @@
+/*Insertion sort is a basic sorting algorithm.
+
+Insertion sort iterates over an array, growing a sorted 
+array behind the current location. It takes each element 
+from the input and finds the spot, up to the current point, 
+where that element belongs (in constant space). It does this 
+until it gets to the end of the array.
+
+Insertion sort should be implemented as a stable sort. This 
+means that equal elements should retain their relative order. 
+Numbers, as primitives, give us no way to check this,so we’ll 
+be sorting objects with a value field, on which they will be 
+sorted, like so:
+
+[{value: 10}, {value: 5, order: 1}, {value: 5, order: 2}]
+A stable sort must return
+
+[{value: 5, order: 1}, {value: 5, order: 2}, {value: 10}]
+in that order.
+
+DO NOT USE Array.sort() method
+
+EXAMPLES:
+[ { "value": 3 }, { "value": 1 }, { "value": 2 } ] 
+=>>> [ { "value": 1 }, { "value": 2 }, { "value": 3 } ]
+
+[ { "value": 10 }, { "value": 5, "order": 1 }, { "value": 5, "order": 2 } ] 
+=>>> [ { "value": 5, "order": 1 }, { "value": 5, "order": 2 }, { "value": 10 } ]
+*/
+
+function insertionSort (array) {
+ // loop through array
+ for (let j = 0; j < array.length; j++) {
+    // loop through array again at one element after array[j]
+    for (let i = j + 1; i < array.length; i++) {
+        // if element value is less than next value
+        if (array[j].value > array[i].value) {
+            // save element
+            let holder = array[j];
+            // swap placements
+            array[j] = array[i];
+            array[i] = holder;
+        }
+    }
+ }
+//  return array
+return array;
+};
+
+
+
+const test1 = insertionSort([ { "value": 3 }, { "value": 1 }, { "value": 2 } ]);
+console.log(`Test1: expected [ { "value": 1 }, { "value": 2 }, { "value": 3 } ] and got ${JSON.stringify(test1)}`);
+const test2 = insertionSort([ { "value": 10 }, { "value": 5, "order": 1 }, { "value": 5, "order": 2 } ]);
+console.log(`Test2: expected [ { "value": 5, "order": 1 }, { "value": 5, "order": 2 }, { "value": 10 } ] and got ${JSON.stringify(test2)}`);
