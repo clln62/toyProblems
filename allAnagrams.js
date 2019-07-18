61d88f78a7aed71427d7036835ee48b38fe89029
Collin Fairlie <collin.fairlie@gmail.com>
2019-07-18 09:28:23 -0500

    Got my function to find all the anagrams of a string using a helper function and recursion.

---
+++ allAnagrams.js
@@ -0,0 +1,44 @@
+/*Given a single input string, write a function that produces 
+all possible anagrams of a string and outputs them as an array. 
+At first, don't worry about repeated strings. What time 
+complexity is your solution?*/
+
+
+function allAnagrams (string) {
+    // create an empty, output array
+    let output = [];
+    // push string into array
+    output.push(string);
+    // split string
+    let split = string.split("");
+    // create a count
+    let count = 1;
+    // helper function that takes in array
+    function helper (array) {
+        // sort split string, join, and save
+        let pushString = array.sort(function() {
+          return .5 - Math.random();
+          }).join("")
+
+        // if output array does not include this version
+        if (!output.includes(pushString)) {
+            // push word to output array
+            output.push(pushString);
+            // increment count
+            count++;
+        }
+    }
+    // create an endpoint
+    let endNum = string.length * 2;
+    // while count is less than endpoint
+    while (count !== endNum) {
+        // call helper function
+        helper(split);
+    }
+    // return output array
+    return output;
+}
+
+console.log(allAnagrams('abc'))
+console.log(allAnagrams('map'))
+console.log(allAnagrams('poor'))
