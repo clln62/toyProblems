398004249caa4cc793ba2786bc81833e7e5142ae
Collin Fairlie <collin.fairlie@gmail.com>
2019-09-27 22:01:50 -0500

    With my helper function, I got this problem to work in quadratic time.

--- highestScoringWord.js
+++ highestScoringWord.js
@@ -18,51 +18,33 @@ https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/javascript
 
 
 function high(x){
-    // const alphabet = '0abcdefghijklmnopqrstuvwxyz';
     const alphabet = {  a:1, b:2, c:3, d:4, e:5, f:6, g:7, h:8, i:9, j: 10, k: 11, l: 12, 
                         m: 13, n: 14, o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, u: 21, 
                         v: 22, w: 23, x: 24, y: 25, z: 26}
-    // create object with each word and point value
     let holder = {};
-    // create holder for individual words
     let wordHolder = '';
-    // create helper function that takes in a string
     const helper = (word) => {
-        // create a number variable set to zero
         let count = 0;
-        // loop through string
         for (let j = 0; j < word.length; j++) {
-            // add point value from alphabet object to count
             count += alphabet[word[j]];
         }
-        // save word with point value in object
         holder[word] = count;
     }
-    // loop through input string
     for (let i = 0; i < x.length; i++) {
-        // place each letter in holder until you reach a space
         if (x[i] !== ' ') {
             wordHolder = wordHolder.concat(x[i]);
         }
-        // if it is a space
         if (x[i] === ' ' || i + 1 === x.length) {
-            // call helper function to get point value on word
             helper(wordHolder);
-            // set holder to empty string
             wordHolder = '';
         }
     }
-    // create output array with string and count
     let output = ['', 0];
-    // loop through object
     for (let key in holder) {
-        // if value is greater one in output
         if (holder[key] > output[1]) {
-            // replace
             output = [key, holder[key]];
         }
     }
-    // return string from output array
     return output[0];
 }
 
