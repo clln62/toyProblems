f2ae2c90228d3b8615ab830c0b4a116327278203
Collin Fairlie <collin.fairlie@gmail.com>
2020-02-16 19:57:13 -0600

    Cleaned up code to remove notes, but they are in previous commit for reference to refactor code at a later date.

--- kebabize.js
+++ kebabize.js
@@ -18,24 +18,16 @@ https://www.codewars.com/kata/57f8ff867a28db569e000c4a/train/javascript
 
 
 function kebabize(str) {
-    // create an output string
     let output = '';
-    // loop through str
     for (let i = 0; i < str.length; i++) {
-        // if letter is lower case
         if (str[i] === str[i].toLowerCase() && str[i].toLowerCase() !== str[i].toUpperCase()) {
-            // add letter to new string
             output = output.concat(str[i]);
         } else if (str[i] === str[i].toUpperCase() && str[i].toLowerCase() !== str[i].toUpperCase() && output.length !== 0) {
-            // if capital letter
-                // add a dash
             output = output.concat('-');
-                // add lower case version of letter to new string
             output = output.concat(str[i].toLowerCase());
         } else if (str[i].toLowerCase() !== str[i].toUpperCase()) {
             output = output.concat(str[i].toLowerCase());
         }
     }
-    // return output string
     return output;
   }
