ece906646e1720e7610331554f28b2fa55ba3c13
Collin Fairlie <collin.fairlie@gmail.com>
2020-04-18 17:45:27 -0500

    Removed pseudo code from working solution.

--- simplePigLatin.js
+++ simplePigLatin.js
@@ -14,31 +14,19 @@ https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
 */
 
 function pigIt(str){
-    // create a result string
     let result = "";
-    // create a temp word string
     let temp = "";
-    // create a holder for the first letter
     let first = "";
-    // loop through string
     for (let i = 0; i < str.length; i++) {
-        // if character is first letter or first character after a space
         if (i === 0 || str[i - 1] === ' ') {
-            // save in first letter holder
             first += str[i];
         } else if (str[i] !== ' ') {
-            // else if character is not a space
-            // place character into temp word string
             temp += str[i];
         } else if (str[i] === ' ' || str[i] === str.length-1) {
-            // else if character is a space or end of input
-            // add first letter with "ay" and a space to result
             result = result.concat(temp.concat(`${first}ay `));
-            // reset temp and first
             temp = '';
             first = '';
         }
     }
-    // return result without last space
     return temp.length === 0 ? result.concat(first) : result.concat(`${temp}${first}ay`);
 }
