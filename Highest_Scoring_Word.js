0b3b225d9b76a36057e42f827d4d82b5d87407dc
Collin Fairlie <collin.fairlie@gmail.com>
2020-05-12 10:15:59 -0500

    Cleaned up my alphebet object.

--- Highest_Scoring_Word.js
+++ Highest_Scoring_Word.js
@@ -17,33 +17,12 @@ https://www.codewars.com/kata/57eb8fcdf670e99d9b000272
 function high(x){
     x = x.concat(' ');
     const alphabet = {
-        a: 1,
-        b: 2,
-        c: 3, 
-        d: 4,
-        e: 5,
-        f: 6,
-        g: 7, 
-        h: 8,
-        i: 9,
-        j: 10,
-        k: 11,
-        l: 12,
-        m: 13,
-        n: 14,
-        o: 15, 
-        p: 16,
-        q: 17,
-        r: 18,
-        s: 19,
-        t: 20,
-        u: 21,
-        v: 22,
-        w: 23,
-        x: 24,
-        y: 25,
-        z: 26
+        a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, 
+        h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14,
+        o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, u: 21,
+        v: 22, w: 23, x: 24, y: 25, z: 26
     }
+    
     let words = [];
     let count = 0;
     let singleWord = '';
